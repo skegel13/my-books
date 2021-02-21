0ff87308f31c8077c4b562229d837e546860df73
Sean Kegel <sean.kegel@hey.com>
2021-02-21 18:58:09 -0500

    Add two columns for mobile

--- components/books-list.tsx
+++ components/books-list.tsx
@@ -13,11 +13,11 @@ function BooksList({title, books}: Props) {
 
   return (
     <>
-      <h2 className="py-2 px-4 mt-4 mb-2 text-sm font-medium tracking-tight text-gray-50 uppercase bg-gray-700 rounded-md shadow-md">
+      <h2 className="px-4 py-2 mt-4 mb-2 text-sm font-medium tracking-tight uppercase bg-gray-700 rounded-md shadow-md text-gray-50">
         {title}
       </h2>
 
-      <ul className="grid grid-cols-1 gap-8 items-end py-4 mb-12 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6">
+      <ul className="grid items-end grid-cols-2 gap-8 py-4 mb-12 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6">
         {books.map(book => (
           <li key={book.id}>
             <Book book={book} />
