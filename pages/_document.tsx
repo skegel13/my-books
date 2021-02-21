d4fbbb30df62d861cc2136f4061330c6eafc4e47
Sean Kegel <sean.kegel@hey.com>
2021-02-21 16:21:57 -0500

    Add courtesy link to Open Library

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,9 +13,18 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
-        <body className="grid grid-rows-1 min-h-screen bg-gradient-to-b from-gray-100 to-gray-300">
+        <body className="grid min-h-screen grid-rows-1 bg-gradient-to-b from-gray-100 to-gray-300">
           <Main />
           <NextScript />
+          <div className="p-4 text-sm text-right">
+            Books provided by{' '}
+            <a
+              className="text-blue-600 hover:text-blue-800"
+              href="https://openlibrary.org/"
+            >
+              Open Library
+            </a>
+          </div>
         </body>
       </Html>
     )
